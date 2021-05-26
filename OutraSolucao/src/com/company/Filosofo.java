67afe6e30a23d1c058a5f60299f6fad738e42889
ramironascimento <40477523+ramironascimento@users.noreply.github.com>
2021-05-26 16:53:31 -0300

    Outra solucao

---
+++ OutraSolucao/src/com/company/Filosofo.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Filosofos {
+    //region Attributes
+    public int idFilosofo;
+    //endregion
+
+    //region Constructor
+
+    public Filosofos(int idFilosofo) {
+        this.idFilosofo = idFilosofo;
+    }
+
+    //endregion
+
+    //region Methods
+
+    //endregion
+}
