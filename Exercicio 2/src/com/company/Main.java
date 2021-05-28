3db25aeb50c92477809a6e84827c55eb0dc0b5b1
ramironascimento <40477523+ramironascimento@users.noreply.github.com>
2021-05-28 18:59:51 -0300

    Bug da ultima fileira corrigido

--- Exercicio 2/src/com/company/Main.java
+++ Exercicio 2/src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Digite o tamanho da matriz");
+        Scanner teclado = new Scanner(System.in);
+        int n = teclado.nextInt();
+        Matriz matriz = new Matriz(n);
+        matriz.matrizToString(matriz.matriz);
+
+        matriz.run();
+
+
     }
 }
---
+++ Exercicio 2/src/com/company/Matriz.java
@@ -0,0 +1,143 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.*;
+
+public class Matriz {
+    //region Attributes
+    public int x = 0;
+    public int y = -1;
+    public int size = 0;
+    public ArrayList<ArrayList<Integer>> matriz = new ArrayList<>();
+    public ArrayList<ArrayList<Integer>> matriz_aux = new ArrayList<>();
+    public CyclicBarrier um_para_dois;
+    public CyclicBarrier dois_para_um;
+    public Semaphore mutex = new Semaphore(1);
+    //endregion
+
+    //region Constructor
+    public Matriz(int n){
+        this.size = n;
+        Random rnd = new Random();
+        for (int i = 0; i < n; i++) {
+            ArrayList<Integer> vetor = new ArrayList<>();
+            ArrayList<Integer> vetor_aux = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                vetor.add(rnd.nextInt(20));
+                vetor_aux.add(0);
+            }
+            matriz.add(vetor);
+            matriz_aux.add(vetor_aux);
+        }
+        um_para_dois = new CyclicBarrier(n*n, parte2);
+        dois_para_um = new CyclicBarrier(1, parte1);
+
+    }
+    //endregion
+
+    //region Methods
+    public void matrizToString(ArrayList<ArrayList<Integer>> m){
+        for (int i = 0; i < m.size(); i++) {
+            System.out.print("# "+(i)+ " # - ");
+            for (int j = 0; j < m.get(i).size(); j++) {
+                if (m.get(j).get(i).toString().length()>1) {
+                    System.out.print(" " + m.get(j).get(i) + " ");
+                }else {
+                    System.out.print(" " + m.get(j).get(i) + "  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    Runnable parte1 = () -> {
+        try {
+            mutex.acquire();
+            int x = getNextX();
+            int y = getNextY();
+            mutex.release();
+            int resultado_soma = matriz.get(x).get(y);
+            //sul
+            if ((y + 1) < matriz.get(x).size()){
+                resultado_soma += matriz.get(x).get(y+1);
+            }
+            //norte
+            if ((y-1) >= 0){
+                resultado_soma += matriz.get(x).get(y-1);
+            }
+            //leste
+            if(x+1 < matriz.size()){
+                resultado_soma += matriz.get(x+1).get(y);
+            }
+
+            //oeste
+            if ((x-1) >= 0){
+                resultado_soma += matriz.get(x-1).get(y);
+            }
+            matriz_aux.get(x).set(y,resultado_soma);
+            await(um_para_dois);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    };
+
+    private synchronized int getNextY() {
+        if (y == -1){
+            y++;
+            return y;
+        }
+        if (y == this.size-1){
+            y= 0;
+            return 0;
+        }
+        y++;
+        return y;
+
+    }
+
+    private synchronized int getNextX() {
+        if(y==this.size-1){
+            if(x==this.size-1){
+                x=0;
+            }else {
+                x++;
+            }
+        }
+        return x;
+    }
+
+    Runnable parte2 = () -> {
+        matriz = matriz_aux;
+        System.out.println("-------");
+        System.out.println("Resultado soma");
+        matrizToString(matriz);
+        System.out.println("-------");
+
+
+    };
+
+
+    private static void await(CyclicBarrier cyclicBarrier) {
+        try {
+            cyclicBarrier.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        ExecutorService executor = Executors.newFixedThreadPool(size*size);
+        for (int i = 0; i < (size * size) + 10; i++) {
+            executor.submit(parte1);
+        }
+        executor.shutdown();
+    }
+
+
+    //endregion
+}
